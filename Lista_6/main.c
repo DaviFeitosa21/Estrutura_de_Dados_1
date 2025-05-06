6a313af8d460b4a6a834bd154819df18015ee658
DaviFeitosa <davisfeitosa21@gmail.com>
2025-05-06 00:28:48 -0300

    inicio da lista 6 + gitignore

---
+++ Lista_6/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 10
+#define TAM_NOME 100
+
+typedef struct{
+    char nome[TAM_NOME];
+    int idade;
+} Pessoa;
+
+typedef struct {
+    Pessoa itens[TAM];
+    int primeiro;
+    int ultimo;
+}Fila;
+
+void iniciar() {
+    
+}
+
+
+
+
+
+
+
+
+
+
